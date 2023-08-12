029ebbf2fffeac49fd1b3fa387bf47393710c6b0
Gaurav verma <94301843+abcdeCoder@users.noreply.github.com>
2023-08-12 16:01:34 +0530

    Update segementtree.cpp

--- segementtree.cpp
+++ segementtree.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 class SGtree{
+public:
   vector<int>sg;
    SGtree(int n){
        sg.resize(4*n +1);
@@ -37,3 +38,45 @@ class SGtree{
        sg[idx] = min(sg[2*idx+1],sg[2*idx+2]);
    }
 };
+int main(){
+    int n1;
+    cin>>n1;
+    vector<int>arr1(n1);
+    for(int i=0;i<n1;i++) cin>>arr1[i];
+    SGtree* s1 = new SGtree(n1);
+    s1->build(0,0,n1-1,arr1);
+    
+    int n2;
+    cin>>n2;
+    vector<int>arr2(n1);
+    for(int i=0;i<n1;i++) cin>>arr2[i];
+    SGtree* s2 = new SGtree(n2);
+    s2->build(0,0,n2-1,arr2);
+    int q;
+    cin>>q;
+    while(q--){
+        int type;
+        cin>>type;
+        if(type ==1){
+            int l1,h1,l2,h2;
+            cin>>l1>>h1>>l2>>h2;
+            int ans1 = s1->qurie(0,0,n1-1,l1,h1);
+            int ans2 = s2->qurie(0,0,n2-1,l2,h2);
+            int ans = min(ans1,ans2);
+            cout<<ans<<endl;
+        }
+        else if(type ==2){
+            int arrno;
+            cin>>arrno;
+            int i,value;
+            cin>>i>>value;
+            if(arrno ==1){
+                s1->update(0,0,n1-1,i,value);
+            }
+            else{
+                 s2->update(0,0,n2-1,i,value);
+            }
+        }
+    }
+    return 0;
+}
